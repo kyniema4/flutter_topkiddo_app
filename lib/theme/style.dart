4c18bb7d7ce9de2a484f0bf0fcc3eece18998bbd
avoca <tran.chaugiangk39@gmail.com>
2020-08-21 15:39:06 +0700

    v1

--- lib/theme/style.dart
+++ lib/theme/style.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'theme.dart' as Theme;
 
 final backgroundImage = BoxDecoration(
   image: DecorationImage(
     image:
-        AssetImage('assets/images/background/BG-iphone-full.jpg'),
+      AssetImage('assets/images/background/BG-iphone-full.jpg'),
     fit: BoxFit.cover,
   ),
 );
@@ -14,7 +15,10 @@ final blackBackground = BoxDecoration(
     colors: [
       Color.fromRGBO(0, 0, 0, 0.2),
       Color.fromRGBO(0, 0, 0, 0.45),
-      Color.fromRGBO(0, 0, 0, 0.75),
+      Color.fromRGBO(0, 0, 0, 0.7),
     ],
   )
 );
+
+final titleBoardHome = TextStyle(color: Theme.Colors.yellow200, fontSize: 13.0, fontFamily: 'UTMCooperBlack');
+final nameBoardHome = TextStyle(color: Theme.Colors.orange500, fontSize: 13.0, fontFamily: 'UTMCooperBlack');
